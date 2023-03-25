88af6f789da15646d461dc804f886a275bfcddb9
Teja <tejendrateja127@gmail.com>
2023-03-25 14:02:37 +0530

    second commit

--- search.py
+++ search.py
@@ -8,7 +8,7 @@ print("Please think of a number between {} and {}".format(low,high))
 input("Press ENTER to start")
 
 guesses = 1
-while True:
+while low != high:
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower"
                      "Enter h or l, or c if my guess is correct : "
@@ -26,3 +26,6 @@ while True:
         print("Please enter h, l or c")
 
     guesses = guesses + 1
+else:
+    print("You thought of the number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
